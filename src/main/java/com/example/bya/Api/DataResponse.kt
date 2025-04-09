2ceac69de8394d1156bd904db45c19f04276d8e3
Venus1an <150965181+Venus1an@users.noreply.github.com>
2025-04-09 15:41:58 -0500

    Rename src/main/java/com/example/bya/DataResponse.kt to src/main/java/com/example/bya/Api/DataResponse.kt

---
+++ src/main/java/com/example/bya/Api/DataResponse.kt
@@ -0,0 +1,3 @@
+package com.example.myapplication.Api
+
+class DataResponse (val message : List < String>)
--- src/main/java/com/example/bya/DataResponse.kt
+++
@@ -1,3 +0,0 @@
-package com.example.myapplication.Api
-
-class DataResponse (val message : List < String>)
